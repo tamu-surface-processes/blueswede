77887da53cb7b8a7b0aab488b35ca6edc63f85f9
Andrew Moodie <amoodie@utexas.edu>
2024-05-30 11:42:34 -0500

    basic testing of filenames for gridder.

---
+++ tests/test_gridding.py
@@ -0,0 +1,64 @@
+# unit tests for gridding
+
+import numpy as np
+
+import pytest
+from pathlib import Path
+
+import unittest.mock as mock
+
+import os
+
+from blueswede import gridding
+
+
+# sample_data_path = os.path.join("test_data", "channel3.sww")
+
+
+@pytest.fixture(scope="module")
+def test_path(request):
+    """Return the directory of the currently running test script"""
+
+    return request.path.parent
+
+
+class Test_files_folders:
+    def test_output_folder(self, tmp_path: Path, test_path) -> None:
+        """Test from the sample data, processing into a dataset."""
+        # set up the paths
+        sample_data_path = test_path.joinpath("test_data", "channel3.sww")
+        _output_path = os.path.join(tmp_path)  # just pass the temp folder, no filename
+
+        # run the gridding
+        gridding.grid_sww_to_netcdf(sww_file=sample_data_path, nc_file=_output_path)
+
+        # check the output
+        assert os.path.exists(os.path.join(_output_path, "channel3.nc"))
+
+    def test_output_folder_filename(self, tmp_path: Path, test_path) -> None:
+        """Test from the sample data, processing into a dataset."""
+        # set up the paths
+        sample_data_path = test_path.joinpath("test_data", "channel3.sww")
+        _output_path = os.path.join(tmp_path, "adifferentfilename.nc")
+
+        # run the gridding
+        gridding.grid_sww_to_netcdf(sww_file=sample_data_path, nc_file=_output_path)
+
+        # check the output
+        assert os.path.exists(os.path.join(tmp_path, "adifferentfilename.nc"))
+        assert not os.path.exists(os.path.join(_output_path, "adifferentfilename.nc"))
+
+
+class Test_nc_formatting:
+    def test_utm(self):
+        """
+        Check the output of a netcdf file that should be UTM referenced.
+        """
+        pass
+
+    def test_arbitrary(self):
+        """
+        Check the output of an arbitraty coordinate system.
+        """
+        # This would be a test for the channel3 data.
+        pass
